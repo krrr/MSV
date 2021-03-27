c5a890443c6350a6fe0080562a46501d680f08a4
krrr <guogaishiwo@gmail.com>
2021-03-27 19:23:55 +0800

    add new preset

--- src/mapscripts/__init__.py
+++ src/mapscripts/__init__.py
@@ -8,12 +8,14 @@ from .fox_tree_top_path import FoxTreeTopPath
 from .fox_tree_lower_path3 import FoxTreeLowerPath3
 from .corridor_h01 import CorridorH01
 from .first_drill_hall import FirstDrillHall
+from .final_edge_of_light4 import Fel4MacroController
 
 
 map_scripts = {
     'cup': CupMacroController,
     'dclp1_shiki': Dclp1MacroController,
     'dclp1_exorcist': Dclp1MacroControllerAlt,
+    'fel4': Fel4MacroController,
     'vc3': Vc3MacroController,
     'tboy_train1': TBoyResearchTrain1,
     'first_drill_hall': FirstDrillHall,
@@ -27,6 +29,7 @@ map2platform = {
     'cup': 'cavern_upper_path',
     'dclp1_shiki': 'deep_cavern_lower_path1',
     'dclp1_exorcist': 'deep_cavern_lower_path1',
+    'fel4': 'final_edge_of_light4',
     'vc3': 'void_current3',
     'tboy_train1': 't_boys_research_train1',
     'first_drill_hall': 'first_drill_hall',
---
+++ src/mapscripts/final_edge_of_light4.py
@@ -0,0 +1,84 @@
+import time
+from macro_script import MacroController
+import directinput_constants as dc
+
+
+# the final edge of light 4 script
+class Fel4MacroController(MacroController):
+    def __init__(self, keymap, logger_queue, cmd_queue):
+        super().__init__(keymap=keymap, log_queue=logger_queue, cmd_queue=cmd_queue)
+        self.last_pickup_money_time = time.time() + 20
+
+    def loop(self):
+        ret = self._loop_common_job()
+        if ret != 0:
+            return ret
+
+        ### Rune Detector
+        self._rune_detect_solve()
+        if not self.current_platform_hash:  # navigate failed, skip rest logic, go unstick fast
+            return
+
+        # set skills
+        if not self.elite_boss_detected and self.set_skills(combine=True):
+            return
+
+        # pickup money
+        if (not self.elite_boss_detected and self.current_platform_hash == 'b73e5168'
+                and time.time() - self.last_pickup_money_time > self.pickup_money_interval):
+            self.pickup_money()
+            self.last_pickup_money_time = time.time()
+            return
+
+        if self.current_platform_hash == 'b73e5168':  # left middle
+            self.player_manager.shiki_exo_shiki(65)
+        elif self.current_platform_hash == 'cb718da9' or self.current_platform_hash == '0ee7b532':
+            self.player_manager.shikigami_haunting_sweep_move(65)
+            if self.current_platform_hash == '0ee7b532':
+                self.player_manager.teleport_up()
+            else:
+                self.player_manager.teleport_down()
+        else:
+            self.navigate_to_platform('b73e5168')  # left middle
+
+        ### Other buffs
+        self.buff_skills(yuki=False)
+        time.sleep(0.05)
+
+        # Finished
+        self.loop_count += 1
+        return 0
+
+    def pickup_money(self):
+        self.logger.info('pick up money')
+        self.navigate_to_platform('0ee7b532')  # bottom
+
+        self.player_manager.shikigami_haunting_sweep_move(self.terrain_analyzer.platforms['0ee7b532'].start_x + 2)
+        time.sleep(0.1)
+        self.check_cmd_queue()
+
+        self.player_manager.shikigami_haunting_sweep_move(self.terrain_analyzer.platforms['e4d256f5'].end_x - 12)  # middle right
+        self.check_cmd_queue()
+
+        self.update()
+        self.navigate_to_platform('e4d256f5')  # middle right
+        self.keyhandler.single_press(dc.DIK_LEFT)
+        self.player_manager.shikigami_haunting()
+        self.player_manager.stay(0.35 + abs(self.player_manager.random_duration(0.1)))
+        self.check_cmd_queue()
+
+        # Quick other player sound notify
+        if self.screen_processor.find_other_player_marker():
+            self.alert_sound()
+
+        self.update()
+        self.navigate_to_platform('2531794a')  # top right
+        self.keyhandler.single_press(dc.DIK_LEFT)
+        self.player_manager.shikigami_haunting()
+        self.player_manager.stay(0.3 + abs(self.player_manager.random_duration(0.1)))
+        self.check_cmd_queue()
+
+        self.update()
+        self.navigate_to_platform('cb718da9')  # top left
+        self.player_manager.shikigami_haunting_sweep_move(self.terrain_analyzer.platforms['cb718da9'].start_x + 5)
+        self.player_manager.stay(0.45 + abs(self.player_manager.random_duration(0.1)))
